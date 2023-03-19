b7fd71373181f6550e4f4a570d4665451ccb9ffa
Nicolas Racchi <nicolas@racchi.com>
2023-03-19 19:41:06 +0100

    feat: v0 done

--- src/bot/answer.rs
+++ src/bot/answer.rs
@@ -1,8 +1,7 @@
-use std::sync::Arc;
-
 use super::Command;
 use crate::prelude::*;
 use rocksdb::DB;
+use std::sync::Arc;
 use teloxide::{prelude::*, utils::command::BotCommands};
 
 pub async fn answer(db: Arc<DB>, bot: Bot, msg: Message, cmd: Command) -> Result<()> {
@@ -19,7 +18,7 @@ pub async fn answer(db: Arc<DB>, bot: Bot, msg: Message, cmd: Command) -> Result
                 msg.chat.id,
                 db.get(constants::TRACKED_KEYWORDS_KEY)?
                     .map(|x| String::from_utf8(x.to_vec()).unwrap())
-                    .unwrap_or_default(),
+                    .unwrap_or_else(|| "No domains are currently tracked.".to_string()),
             )
             .await?
         }
@@ -27,8 +26,12 @@ pub async fn answer(db: Arc<DB>, bot: Bot, msg: Message, cmd: Command) -> Result
         // Add a keyword to the list of tracked keywords.
         Command::Track(domain) => {
             let mut domains = db.get(constants::TRACKED_KEYWORDS_KEY)?.unwrap_or_default();
-            domains.extend_from_slice(f!(",{}", domain).as_bytes());
+
+            // Add a comma if there are already domains in the list
+            let should_add_comma = if !domains.is_empty() { "," } else { "" };
+            domains.extend_from_slice(f!("{}{}", should_add_comma, domain).as_bytes());
             db.put(constants::TRACKED_KEYWORDS_KEY, domains)?;
+
             bot.send_message(msg.chat.id, "Added domain to tracking list.")
                 .await?
         }
