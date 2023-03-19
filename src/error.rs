e59e883714d5c6c3b1f124b5bf04f9bb102ebe97
Nicolas Racchi <nicolas@racchi.com>
2023-03-19 16:33:27 +0100

    chore: simple teloxide bot

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,20 @@
+use std::convert::Infallible;
+use teloxide::dispatching::dialogue::SqliteStorageError;
+
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error("Generic {0}")]
     Generic(String), // TODO: Remove for release
 
-    #[error(transparent)]
+    #[error("IO error: {0}")]
     IO(#[from] std::io::Error),
+
+    #[error("Teloxide error: {0}")]
+    Teloxide(#[from] teloxide::RequestError),
+
+    #[error("Sqlite error: {0}")]
+    Sqlite(#[from] SqliteStorageError<Infallible>),
+
+    #[error("Teloxide handler error: {0}")]
+    TeloxideHandler(#[from] Box<dyn std::error::Error + Send + Sync>),
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,16 @@ mod error;
 mod prelude;
 mod utils;
 
-use teloxide::{prelude::*, utils::command::BotCommands};
+use std::time::Duration;
+use teloxide::{
+    dispatching::dialogue::{serializer::Json, ErasedStorage, SqliteStorage, Storage},
+    prelude::*,
+    types::Recipient,
+    utils::command::BotCommands,
+};
+use tokio::join;
+
+const GROUP_CHAT_ID: &str = "-900218105";
 
 #[derive(BotCommands, Clone)]
 #[command(
@@ -18,17 +27,28 @@ enum Command {
     Help,
     #[command(description = "list tracked domain names.")]
     Domains,
+    #[command(description = "get the current chat id.")]
+    ChatId,
 }
+
+#[derive(Clone, Default, serde::Serialize, serde::Deserialize)]
+enum State {
+    #[default]
+    Domains,
+}
+
+type BotStorage = std::sync::Arc<ErasedStorage<State>>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv::dotenv().ok();
     pretty_env_logger::init();
-    log::info!("Starting throw dice bot...");
+    log::info!("Starting bot...");
 
     let bot = Bot::from_env();
+    let bot_storage: BotStorage = SqliteStorage::open("db.sqlite", Json).await?.erase();
 
-    Command::repl(bot, answer).await;
+    join!(Command::repl(bot.clone(), answer), send_update(bot.clone()));
 
     Ok(())
 }
@@ -40,7 +60,22 @@ async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
                 .await?
         }
         Command::Domains => bot.send_message(msg.chat.id, f!("test")).await?,
+        Command::ChatId => bot.send_message(msg.chat.id, f!("{}", msg.chat.id)).await?,
     };
+
+    Ok(())
+}
+
+async fn send_update(bot: Bot) -> Result<()> {
+    loop {
+        tokio::time::sleep(Duration::from_secs(5)).await;
+
+        // 1. check if there are any new domains in rocksdb that match the tracked domains
+        // 2. if there are, send a new message to the chat
+
+        bot.send_message(Recipient::from(GROUP_CHAT_ID.to_owned()), "test")
+            .await?;
+    }
 
     Ok(())
 }
